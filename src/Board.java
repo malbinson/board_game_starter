17af6557c49adfd3b6f68c5a2cd8ebb091aa206b
malbinson <mattalbinson@berkeley.net>
2020-03-09 11:10:34 -0700

    Adding more info for connect-4

--- src/Board.java
+++ src/Board.java
@@ -18,30 +18,34 @@ public class Board {
 
     }
 
-    //printBoard will output a board to the console
+    //************ ALGORITHM #1 ***************
+    //displayBoard will output a board to the console
     public void printBoard() {
         System.out.println("Updated board:");
         for(int row = 0; row < size; row++) {
             for(int col = 0; col < size; col++) {
 
-                System.out.print(grid[row][col] + " ");
+                System.out.print(grid[row][col] + " ");  //prints out each value in each array
 
             }
-            System.out.print("\n");
+            System.out.print("\n");  // "\n" is the character for "new line"
         }
 
 
     }
 
+    //************ ALGORITHM #2 ***************
     //makes a move on the board - changes a position to "token value"
     public void makeMove(int row, int col, String token) {
 
-        grid[row][col] = token;
+        grid[row][col] = token;  // for Connect-4 you will need to determine the row value
+                                 // based on the existing values in the grid (where does the token "drop" to)
     }
 
 
-    //win check routine with basic "algorithm" for now
-    //any move at 0,0 ends game
+    //************ ALGORITHM #3 ***************
+    // return true if there are 4 in a row anywhere on the board
+    // for now, any move at 0,0 ends game
     public boolean checkWin() {
 
         if(grid[0][0] != "0") {
--- src/Computer.java
+++ src/Computer.java
@@ -1,10 +1,13 @@
 public class Computer extends Player {
 
     public Computer(String token) {
+
         super("Bit Bucket", token);
     }
 
-    //very basic random move algorithm for now
+    //************ ALGORITHM #4 ***************
+    // very basic random move algorithm for now
+    // how should the computer decide how to move?
     public int[] determineMove() {
 
         int row = (int )(Math.random() * 8);
